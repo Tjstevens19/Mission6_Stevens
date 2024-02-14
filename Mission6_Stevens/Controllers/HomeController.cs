14bba49b9990919950a1e2951e9d42f2b416f38e
Tjstevens19 <tdogstrike@gmail.com>
2024-02-14 13:19:49 -0700

    Add project files.

---
+++ Mission6_Stevens/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+using Microsoft.AspNetCore.Mvc;
+//using Mission6_Stevens.Models;
+using System.Diagnostics;
+
+namespace Mission6_Stevens.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult GetToKnowJoel()
+        {
+            return View();
+        }
+
+    }
+}
