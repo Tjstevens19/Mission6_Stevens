f79ca616b715af40987e22f439bf3fa2fc1eb00c
Tjstevens19 <tdogstrike@gmail.com>
2024-02-15 17:20:14 -0700

    update AddMovie model

--- Mission6_Stevens/Models/AddMovie.cs
+++ Mission6_Stevens/Models/AddMovie.cs
@@ -3,47 +3,16 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace Mission6_Stevens.Models
 {
-    public class AddMovieModel : PageModel
+    public class AddMovieModel
     {
-        [BindProperty]
+        public string Category { get; set; }
         public string Title { get; set; }
-
-        [BindProperty]
+        public int Year { get; set; }
         public string Director { get; set; }
-
-        [BindProperty]
-        public string Genre { get; set; }
-
-        [BindProperty]
-        public int ReleaseYear { get; set; }
-
-        [BindProperty]
         public string Rating { get; set; }
+        public bool? Edited { get; set; }
+        public string? LentTo { get; set; }
+        public string? Notes { get; set; }
 
-        [BindProperty]
-        public bool Edited { get; set; }
-
-        [BindProperty]
-        public string LentTo { get; set; }
-
-        [BindProperty]
-        public string Notes { get; set; }
-
-        public void OnGet()
-        {
-            // This method is called when the page is initially requested.
-        }
-
-        public IActionResult OnPost()
-        {
-            // This method is called when the form is submitted.
-            // Handle the form submission logic here (e.g., save to a database).
-
-            // Example: Save the movie data to a database
-            // MovieDatabase.AddMovie(new Movie { Title = Title, Director = Director, ... });
-
-            // Redirect to a confirmation page or the list of movies
-            return RedirectToPage("/Movies/Index");
-        }
     }
 }
