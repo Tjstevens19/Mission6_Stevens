545c5977c6a7c3b41ac760d4b1c9f5bb49242714
Tjstevens19 <tdogstrike@gmail.com>
2024-02-21 18:55:20 -0700

    get ViewMovies working, change Model names and add Category, set up new database, add proper routing in HomeController

--- Mission6_Stevens/Controllers/HomeController.cs
+++ Mission6_Stevens/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Mission6_Stevens.Models;
 using System.Diagnostics;
 
@@ -6,8 +7,8 @@ namespace Mission6_Stevens.Controllers
 {
     public class HomeController : Controller
     {
-        private AddMovieModelContext _context;
-        public HomeController(AddMovieModelContext context)
+        private MovieContext _context;
+        public HomeController(MovieContext context) // Constructor
         {
             _context = context;
         }
@@ -24,14 +25,82 @@ namespace Mission6_Stevens.Controllers
         [HttpGet]
         public IActionResult AddMovie()
         {
-            return View();
+            ViewBag.Categories = _context.Categories
+                .OrderBy(x => x.CategoryName)
+                .ToList();
+
+            return View("AddMovie", new Movie());
         }
         [HttpPost]
-        public IActionResult AddMovie(AddMovieModel response)
+        public IActionResult AddMovie(Movie response)
         {
-            _context.Movies.Add(response); // Add the new movie to the database
-            _context.SaveChanges(); // Save the changes
-            return View("Confirmation", response);
+            if (ModelState.IsValid)
+            {
+                _context.Movies.Add(response); // Add the new movie to the database
+                _context.SaveChanges(); // Save the changes
+                return View("Confirmation", response);
+            }
+            else // Invalid data
+            {
+                ViewBag.Categories = _context.Categories
+                    .OrderBy(x => x.CategoryName)
+                    .ToList();
+                return View("AddMovie", response);
+            }
+
         }
+        [HttpGet]
+        public IActionResult ViewMovies()
+        {
+            //Linq Query
+            var movies = _context.Movies
+                .Include(x => x.Category)
+                .OrderBy(x => x.Title)
+                .ToList();
+            return View(movies);
+
+        }
+        //[HttpGet]
+        //public IActionResult Edit(int id)
+        //{
+        //    var recordToEdit = _context.Movies
+        //        .Single(x => x.MovieId == id);
+
+        //    ViewBag.Categories = _context.Categories
+        //        .OrderBy(x => x.CategoryName)
+        //        .ToList();
+        //    return View("DatingApplication", recordToEdit);
+        //}
+        //[HttpPost]
+        //public IActionResult Edit(AddMovieModel response)
+        //{
+        //    if (ModelState.IsValid)
+        //    {
+        //        _context.Movies.Update(response);
+        //        _context.SaveChanges();
+        //        return View("Confirmation", response);
+        //    }
+        //    else
+        //    {
+        //        ViewBag.Categories = _context.Categories
+        //            .OrderBy(x => x.CategoryName)
+        //            .ToList();
+        //        return View("DatingApplication", response);
+        //    }
+        //}
+        //[HttpGet]
+        //public IActionResult Delete(int id)
+        //{
+        //    var recordToDelete = _context.Movies
+        //        .Single(x => x.MovieId == id);
+        //    return View(recordToDelete);
+        //}
+        //[HttpPost]
+        //public IActionResult Delete(AddMovieModel response)
+        //{
+        //    _context.Movies.Remove(response);
+        //    _context.SaveChanges();
+        //    return RedirectToAction("ViewMovies");
+
     }
 }
--- Mission6_Stevens/Models/Movie.cs
+++ Mission6_Stevens/Models/Movie.cs
@@ -1,22 +1,25 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Mission6_Stevens.Models
 {
-    public class AddMovieModel
+    public class Movie
     {
         [Key]
         [Required]
         public int MovieId { get; set; }
-        public string Category { get; set; }
-        public string? Subcategory { get; set; }
+        [ForeignKey("CategoryId")]
+        public int? CategoryId { get; set; }
+        public Category? Category { get; set; }
         public string Title { get; set; }
         public int Year { get; set; }
-        public string Director { get; set; }
-        public string Rating { get; set; }
-        public bool? Edited { get; set; }
+        public string? Director { get; set; }
+        public string? Rating { get; set; }
+        public bool Edited { get; set; }
         public string? LentTo { get; set; }
+        public string CopiedToPlex { get; set; }
         [StringLength(25)]
         public string? Notes { get; set; }
 
